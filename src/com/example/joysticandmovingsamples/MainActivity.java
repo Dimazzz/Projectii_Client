dfd605d15de8df8b5affa5400ac160658fb4ccc1
Dmitry <diman_gut@mail.ru>
2012-11-02 14:58:15 +0400

    Move_Samples v0.21

--- src/com/example/joysticandmovingsamples/MainActivity.java
+++ src/com/example/joysticandmovingsamples/MainActivity.java
@@ -26,6 +26,7 @@ import org.andengine.opengl.texture.region.TextureRegion;
 import org.andengine.ui.activity.SimpleBaseGameActivity;
 import org.andengine.util.math.MathUtils;
 
+import android.content.res.Resources;
 import android.opengl.GLES20;
 
 import com.badlogic.gdx.math.Vector2;
@@ -36,8 +37,8 @@ import com.badlogic.gdx.physics.box2d.FixtureDef;
 
 public class MainActivity extends SimpleBaseGameActivity {
 	
-	private static final int CAMERA_WIDTH = 480;
-	private static final int CAMERA_HEIGHT = 320;
+	private static  int CAMERA_WIDTH ;
+	private static  int CAMERA_HEIGHT;
 	
 	private Camera mCamera;
 	
@@ -56,6 +57,10 @@ public class MainActivity extends SimpleBaseGameActivity {
 
 	
 	public EngineOptions onCreateEngineOptions() {
+		Resources res = getResources();
+		CAMERA_HEIGHT = res.getDisplayMetrics().heightPixels;
+		CAMERA_WIDTH = res.getDisplayMetrics().widthPixels;
+		
 		this.mCamera = new Camera(0, 0, CAMERA_WIDTH, CAMERA_HEIGHT);
 
 		return new EngineOptions(true, ScreenOrientation.LANDSCAPE_FIXED, new RatioResolutionPolicy(CAMERA_WIDTH, CAMERA_HEIGHT), this.mCamera);
@@ -129,15 +134,11 @@ public class MainActivity extends SimpleBaseGameActivity {
 			public void onControlChange(final BaseOnScreenControl pBaseOnScreenControl, final float pValueX, final float pValueY) {
 				final Body carBody = MainActivity.this.ShipBody;
 
-				final Vector2 velocity = Vector2Pool.obtain(pValueX * 5, pValueY * 5);
+				final Vector2 velocity = Vector2Pool.obtain(pValueX * 8, pValueY * 8);
 				carBody.setLinearVelocity(velocity);
 				Vector2Pool.recycle(velocity);
-				if(pValueX == x1 && pValueY == x1) {
+				if(!(pValueX == x1 && pValueY == x1)) {
 
-					carBody.setTransform(carBody.getWorldCenter(), x1);
-
-					MainActivity.this.Ship.setRotation(MathUtils.radToDeg(x1));
-				} else {
 
 					carBody.setTransform(carBody.getWorldCenter(),(float)Math.atan2(pValueX, -pValueY) );
 
@@ -153,7 +154,10 @@ public class MainActivity extends SimpleBaseGameActivity {
 			}
 		});
 		analogOnScreenControl.getControlBase().setBlendFunction(GLES20.GL_SRC_ALPHA, GLES20.GL_ONE_MINUS_SRC_ALPHA);
-		analogOnScreenControl.getControlBase().setAlpha(0.5f);
+		analogOnScreenControl.getControlBase().setAlpha(0.5f);   //прозрачность
+		analogOnScreenControl.getControlBase().setScaleCenter(-20, 128);   // пололжение скрин контрола 
+		analogOnScreenControl.getControlBase().setScale(1.75f);   // размер большой картинки
+		analogOnScreenControl.getControlKnob().setScale(1.75f);  // размер окружности в центре 
 		
 		analogOnScreenControl.refreshControlKnobPosition();
 		
