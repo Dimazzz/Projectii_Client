38951fb945d345653b885ab85aa2b138b8ef8f2d
Yau Ref <yau.ref@gmail.com>
2012-12-20 20:30:47 +0400

    Finaly, it's worling as I want

--- src/org/projii/client/net/BasicCoordinationServerConnection.java
+++ src/org/projii/client/net/BasicCoordinationServerConnection.java
@@ -1,11 +1,14 @@
 package org.projii.client.net;
 
-import org.jai.BSON.BSONDecoder;
-import org.jai.BSON.BSONDocument;
-import org.jai.BSON.BSONEncoder;
+import org.jai.BSON.*;
 import org.projii.commons.GameInfo;
 import org.projii.commons.net.CoordinationServerRequests;
+import org.projii.commons.net.CoordinationServerResponses;
 import org.projii.commons.spaceship.Spaceship;
+import org.projii.commons.spaceship.SpaceshipModel;
+import org.projii.commons.spaceship.equipment.*;
+import org.projii.commons.spaceship.weapon.Weapon;
+import org.projii.commons.spaceship.weapon.WeaponModel;
 
 import java.io.ByteArrayOutputStream;
 import java.io.InputStream;
@@ -18,12 +21,8 @@ import java.util.List;
 public class BasicCoordinationServerConnection implements CoordinationServerConnection {
 
     private Socket socket;
-    private String address;
-    private int port;
 
     public BasicCoordinationServerConnection(String address, int port) {
-        this.address = address;
-        this.port = port;
         try {
             socket = new Socket(address, port);
         } catch (Exception x) {
@@ -39,15 +38,14 @@ public class BasicCoordinationServerConnection implements CoordinationServerConn
             sout.write(BSONEncoder.encode(doc).array());
             int read_length;
             byte buf[] = new byte[1024];
-            int time = 0;
-            Thread.currentThread();
 
-            while (sin.available() == 0) {
-                if (time > 10000)
-                    return null;
-                Thread.sleep(1000);
-                time += 1000;
-            }
+//            int time = 0;
+//            while (sin.available() == 0) {
+//                if (time > 10000)
+//                    return null;
+//                Thread.sleep(1000);
+//                time += 1000;
+//            }
 
             ByteArrayOutputStream baos = new ByteArrayOutputStream();
             do {
@@ -80,11 +78,12 @@ public class BasicCoordinationServerConnection implements CoordinationServerConn
     public List<Spaceship> getMyShips() {
         List<Spaceship> ships = null;
         BSONDocument request = new BSONDocument();
-        request.add("type", 2);
+        request.add("type", CoordinationServerRequests.GET_MY_SHIPS_FULL);
         try {
             BSONDocument response = executeRequest(request);
             if (response == null)
                 return null;
+
             ships = convertShipData(response);
         } catch (Exception x) {
             x.printStackTrace();
@@ -93,61 +92,158 @@ public class BasicCoordinationServerConnection implements CoordinationServerConn
     }
 
     private List<Spaceship> convertShipData(BSONDocument response) {
-        LinkedList<Spaceship> ships = new LinkedList<Spaceship>();
-//        BSONDocument ship_doc, weaponMas_doc, model_doc, generator_doc, engine_doc, shield_doc, weapon_doc, bullet_doc, location_doc;
-//        Weapon[] weaponMas;
-//        SpaceshipModel model;
-//        EnergyGenerator generator;
-//        SpaceshipEngine engine;
-//        EnergyShield shield;
-//        Bullet bullet;
-//        Point location;
-//        for (int i = 0; i < response.size() - 1; i++) {
-//            ship_doc = (BSONDocument) response.get(Integer.toString(i));
-//            model_doc = (BSONDocument) ship_doc.get("model");
-//            generator_doc = (BSONDocument) ship_doc.get("generator");
-//            engine_doc = (BSONDocument) ship_doc.get("engine");
-//            shield_doc = (BSONDocument) ship_doc.get("energyShield");
-//            weaponMas_doc = (BSONDocument) ship_doc.get("weapons");
-//            generator = new EnergyGenerator((int) generator_doc.get("id"),
-//                    (int) generator_doc.get("maxCount"),
-//                    (int) generator_doc.get("regeneration"));
-//            engine = new SpaceshipEngine((int) engine_doc.get("id"),
-//                    (int) engine_doc.get("maxSpeed"),
-//                    (int) engine_doc.get("speedUp"),
-//                    (int) engine_doc.get("handleability"));
-//            shield = new EnergyShield((int) shield_doc.get("id"),
-//                    (int) shield_doc.get("maxCount"),
-//                    (int) shield_doc.get("regeneration"),
-//                    (int) shield_doc.get("time"));
-//            model = new SpaceshipModel((String) model_doc.get("name"),
-//                    (int) model_doc.get("id"),
-//                    (int) model_doc.get("health"),
-//                    (int) model_doc.get("armor"),
-//                    (int) model_doc.get("weaponAmount"),
-//                    (int) model_doc.get("length"),
-//                    (int) model_doc.get("width"));
-//            weaponMas = new Weapon[weaponMas_doc.size()];
-//            for (int j = 0; j < weaponMas_doc.size(); j++) {
-//                weapon_doc = (BSONDocument) weaponMas_doc.get(Integer.toString(j));
-//                bullet_doc = (BSONDocument) weapon_doc.get("bullet");
-//                location_doc = (BSONDocument) bullet_doc.get("location");
-//                location = new Point((int) location_doc.get("x"), (int) location_doc.get("y"));
-//                bullet = new Bullet((int) bullet_doc.get("id"),
-//                        (int) bullet_doc.get("speed"),
-//                        (int) bullet_doc.get("damage"),
-//                        (int) bullet_doc.get("consumeEnergy"),
-//                        (int) bullet_doc.get("distance"),
-//                        (int) bullet_doc.get("range"),
-//                        location,
-//                        (int) bullet_doc.get("rotation"),
-//                        (int) bullet_doc.get("length"),
-//                        (int) bullet_doc.get("width"));
-//                weaponMas[j] = new Blaster((int) weapon_doc.get("id"), bullet, (int) weapon_doc.get("rate"));
-//            }
-//            ships.add(new Spaceship(model, generator, engine, shield, weaponMas));
-//        }
+        List<Spaceship> ships = new LinkedList<>();
+        int count, id;
+
+        SpaceshipModel shipModel = null;
+        Weapon[] shipWeapons;
+        SpaceshipEngine shipEngine = null;
+        EnergyShield shipShield = null;
+        EnergyGenerator shipGenerator = null;
+
+        List<SpaceshipModel> spaceshipModels = getSpaceshipModels(response);
+        List<WeaponModel> weaponModels = getWeaponModels(response);
+        List<SpaceshipEngine> engineModels = getSpaceshipEngineModels(response);
+        List<EnergyShieldModel> shieldModels = getEnergyShieldModels(response);
+        List<EnergyGeneratorModel> generatorModels = getEnergyGeneratorModels(response);
+
+        BSONDocument spaceships = (BSONDocument) response.get("spaceships");
+        BSONDocument spaceship, weapons;
+        List<Integer> weaponsId;
+
+        for (int i = 0; i < spaceships.size(); i++) {
+            spaceship = (BSONDocument) spaceships.get(i + "");
+            weapons = (BSONDocument) spaceship.get("weapons");
+            weaponsId = new LinkedList<>();
+            for (count = 0; count < weapons.size() - 1; count++) {
+                weaponsId.add((Integer) weapons.get(count + ""));
+            }
+            shipWeapons = new Weapon[weaponsId.size() - 1];
+
+            for (count = 0, id = (int) spaceship.get("modelId"); count < spaceshipModels.size(); count++) {
+                if (id == spaceshipModels.get(count).id) {
+                    shipModel = spaceshipModels.get(count);
+                    break;
+                }
+            }
+            for (count = 0, id = (int) spaceship.get("engineId"); count < engineModels.size(); count++) {
+                if (id == engineModels.get(count).getId()) {
+                    shipEngine = engineModels.get(count);
+                    break;
+                }
+            }
+            for (count = 0, id = (int) spaceship.get("shieldId"); count < shieldModels.size(); count++) {
+                if (id == shieldModels.get(count).id) {
+                    shipShield = new EnergyShield(shieldModels.get(count));
+                    break;
+                }
+            }
+            for (count = 0, id = (int) spaceship.get("generatorId"); count < generatorModels.size(); count++) {
+                if (id == generatorModels.get(count).id) {
+                    shipGenerator = new EnergyGenerator(generatorModels.get(count));
+                    break;
+                }
+            }
+            for (int j = 0; j < weaponsId.size(); j++) {
+                for (count = 0, id = weaponsId.get(j); count < weaponModels.size() - 1; count++) {
+                    if (id == weaponModels.get(count).getId()) {
+                        shipWeapons[j] = new Weapon(weaponModels.get(count));
+                        break;
+                    }
+                }
+            }
+
+            id = (int) spaceship.get("id");
+            ships.add(new Spaceship(id, shipModel, shipWeapons, shipGenerator, shipEngine, shipShield));
+        }
         return ships;
+    }
+
+    private List<WeaponModel> getWeaponModels(BSONDocument response) {
+        List<WeaponModel> weaponModels = new LinkedList<>();
+        BSONDocument wm;
+        BSONDocument weaponModelsInfo = (BSONDocument) response.get("weapons");
+        for (int i = 0; i < weaponModelsInfo.size(); i++) {
+            wm = (BSONDocument) weaponModelsInfo.get(i + "");
+            int weaponModelId = (int) wm.get("id");
+            String name = (String) wm.get("name");
+            int rate = (int) wm.get("rate");
+            int weaponType = (int) wm.get("type");
+            int bulletSpeed = (int) wm.get("bulletSpeed");
+            int damage = (int) wm.get("damage");
+            int energyConsumption = (int) wm.get("energyConsumption");
+            int distance = (int) wm.get("distance");
+            int range = (int) wm.get("range");
+            int cooldown = (int) wm.get("cooldown");
+            weaponModels.add(new WeaponModel(weaponModelId, name, rate, weaponType, bulletSpeed, damage, energyConsumption,
+                    distance, range, cooldown));
+        }
+        return weaponModels;
+    }
+
+    private List<SpaceshipModel> getSpaceshipModels(BSONDocument response) {
+        List<SpaceshipModel> spaceShipModels = new LinkedList<>();
+        BSONDocument spaceshipModelInfo;
+        BSONDocument spaceshipModelsInfo = (BSONDocument) response.get("spaceshipModels");
+        for (int i = 0; i < spaceshipModelsInfo.size(); i++) {
+            spaceshipModelInfo = (BSONDocument) spaceshipModelsInfo.get(i + "");
+            int spaceshipModelId = (int) spaceshipModelInfo.get("id");
+            String spaceshipModelName = (String) spaceshipModelInfo.get("name");
+            int health = (int) spaceshipModelInfo.get("health");
+            int width = (int) spaceshipModelInfo.get("width");
+            int length = (int) spaceshipModelInfo.get("length");
+            int armor = (int) spaceshipModelInfo.get("armor");
+            int weaponSlotCount = (int) spaceshipModelInfo.get("weaponSlotCount");
+            spaceShipModels.add(new SpaceshipModel(spaceshipModelId, spaceshipModelName, length, width, health, weaponSlotCount, armor));
+        }
+        return spaceShipModels;
+    }
+
+    private List<SpaceshipEngine> getSpaceshipEngineModels(BSONDocument response) {
+        List<SpaceshipEngine> engineModels = new LinkedList<>();
+        BSONDocument engineModel;
+        BSONDocument engineModelsInfo = (BSONDocument) response.get("engineModels");
+        for (int i = 0; i < engineModelsInfo.size(); i++) {
+            engineModel = (BSONDocument) engineModelsInfo.get(i + "");
+            int engineModelId = (int) engineModel.get("id");
+            int maneuverability = (int) engineModel.get("maneuverability");
+            int maxSpeed = (int) engineModel.get("maxSpeed");
+            int acceleration = (int) engineModel.get("acceleration");
+            String engineModelName = (String) engineModel.get("name");
+            engineModels.add(new SpaceshipEngine(engineModelId, maxSpeed, acceleration, maneuverability, engineModelName));
+        }
+        return engineModels;
+    }
+
+    private List<EnergyShieldModel> getEnergyShieldModels(BSONDocument response) {
+        List<EnergyShieldModel> shieldModels = new LinkedList<>();
+        BSONDocument shieldModel;
+        BSONDocument shieldModelsInfo = (BSONDocument) response.get("shieldModels");
+        for (int i = 0; i < shieldModelsInfo.size(); i++) {
+            shieldModel = (BSONDocument) shieldModelsInfo.get(i + "");
+            int shieldModelId = (int) shieldModel.get("id");
+            int maxEnergyLevel = (int) shieldModel.get("maxEnergyLevel");
+            int regenerationDelay = (int) shieldModel.get("regenerationDelay");
+            int regenerationSpeed = (int) shieldModel.get("regenerationSpeed");
+            String shieldModelName = (String) shieldModel.get("name");
+            shieldModels.add(new EnergyShieldModel(shieldModelId, shieldModelName, maxEnergyLevel, regenerationSpeed, regenerationDelay));
+        }
+        return shieldModels;
+    }
+
+    private List<EnergyGeneratorModel> getEnergyGeneratorModels(BSONDocument response) {
+        List<EnergyGeneratorModel> generatorModels = new LinkedList<>();
+        BSONDocument generatorModel;
+        BSONDocument generatorModelsInfo = (BSONDocument) response.get("generatorModels");
+        for (int i = 0; i < generatorModelsInfo.size(); i++) {
+            generatorModel = (BSONDocument) generatorModelsInfo.get(i + "");
+            int modelId = (int) generatorModel.get("id");
+            int maxEnergyLevel = (int) generatorModel.get("maxEnergyLevel");
+            int regenerationSpeed = (int) generatorModel.get("regenerationSpeed");
+            String generatorModelName = (String) generatorModel.get("name");
+            generatorModels.add(new EnergyGeneratorModel(modelId, generatorModelName, maxEnergyLevel, regenerationSpeed));
+        }
+        return generatorModels;
     }
 
     public List<GameInfo> getGamesList() {
@@ -156,24 +252,15 @@ public class BasicCoordinationServerConnection implements CoordinationServerConn
         request.add("type", CoordinationServerRequests.GET_GAMES);
         try {
             BSONDocument response = executeRequest(request);
-            if (response == null) {
+            if (response == null || response.get("type") != CoordinationServerResponses.GAMES_INFO) {
                 return null;
             }
-
             BSONDocument document = (BSONDocument) response.get("games");
 
-            BSONDocument gameInfo;
-            games = new LinkedList<GameInfo>();
-            for (int i = 0; i < document.size(); i++) {
-                gameInfo = (BSONDocument) document.get(Integer.toString(i));
-
-                int gameId = (int) gameInfo.get("gameId");
-                String serverIP = (String) gameInfo.get("serverIP");
-                String mapName = (String) gameInfo.get("mapName");
-                int currentPlayersAmount = (int) gameInfo.get("currentPlayersAmount");
-                int maxPlayersAmount = (int) gameInfo.get("maxPlayersAmount");
-
-                games.add(new GameInfo(gameId, serverIP, mapName, currentPlayersAmount, maxPlayersAmount));
+            games = new LinkedList<>();
+            for (BSONDocumentElement bsonDocumentElement : document) {
+                GameInfo gameInfo = BSONSerializer.deserialize(GameInfo.class, (BSONDocument) bsonDocumentElement.getValue());
+                games.add(gameInfo);
             }
         } catch (Exception x) {
             x.printStackTrace();
